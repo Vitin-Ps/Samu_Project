e3f3997a325721f56cb34a31ec69e5b448251a6c
Vitin-Ps <vicssoares2802@gmail.com>
2024-12-28 11:47:17 -0300

    inicio API

---
+++ API/SAMU_API/src/main/java/com/example/SAMU_API/SamuApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.SAMU_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamuApiApplication.class, args);
+	}
+
+}
---
+++ API/SAMU_API/src/test/java/com/example/SAMU_API/SamuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SAMU_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamuApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
