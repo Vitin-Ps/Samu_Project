9424472f3010e5b38f4b7f74ad826ec61756861b
victor.soares <victor.soares@inv.net.br>
2025-01-05 12:34:03 -0300

    Removendo GlueStack

---
+++ MOBILE/SAMU_MOBILE/src/App.tsx
@@ -0,0 +1,12 @@
+import {KeyboardAvoidingView, StatusBar} from 'react-native';
+import Routes from './Routes';
+
+const App = () => {
+  return (
+   <KeyboardAvoidingView>
+    <Routes/>
+   </KeyboardAvoidingView>
+  );
+};
+
+export default App;
---
+++ MOBILE/SAMU_MOBILE/src/screens/Home.tsx
@@ -0,0 +1,11 @@
+import {KeyboardAvoidingView, Text} from 'react-native';
+
+const Home = () => {
+  return (
+    <KeyboardAvoidingView>
+      <Text>Hello World</Text>
+    </KeyboardAvoidingView>
+  );
+};
+
+export default Home;
